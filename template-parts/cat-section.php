580183776d5fe35770cde3965c5b71baf8dc9ac5
Mauro <mauro@mojahmedia.net>
2019-03-22 22:46:22 -0300

    removing commented code

--- template-parts/cat-section.php
+++ template-parts/cat-section.php
@@ -3,13 +3,6 @@
  * testing version 0.2.1
  */
 ?>
-<!--<div class="section-cat-container__inner grid-x grid-margin-x">-->
-
-<!--<div class="section-cat-container__inner medium-6">
-<div class="section-header" style="width:100%">
-    <h2><a title="Seel all content related to '<?php echo $term_data->name; ?>'" href="<?php echo get_term_link($term_data); ?>"><?php echo $term_name ?></a></h2>
-</div>-->
-
 <?php
     $posts = get_posts(array(
         'posts_per_page' => 4,
