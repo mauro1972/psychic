217009f7d1f29dfeab5e85e10438c1943b77fd5c
Mauro <mauro@mojahmedia.net>
2019-05-04 22:12:59 -0300

    removing default content

--- category.php
+++ category.php
@@ -87,7 +87,7 @@ get_header(); ?>
                 <?php endwhile; ?>
             </div>
 			<?php else : ?>
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php //get_template_part( 'template-parts/content', 'none' ); ?>
 
 			<?php endif; // End have_posts() check. ?>
 
